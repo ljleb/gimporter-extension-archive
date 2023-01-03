ce935042098ca78bd8d73c9cd502558ddaa31b39
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-01-03 16:39:11 -0500

    started working on webui script

--- gimp_plugins/SD_inpaint.py
+++ gimp_plugins/SD_inpaint.py
@@ -4,7 +4,7 @@ from gimpfu import *
 import gimpcolor
 import os
 
-working_dir = r'C:\Users\Plads\Documents\GitHub\stable diffusion\stable-diffusion-avidhjeren\extensions\gimp_inpainter\gimp_plugins'
+working_dir = os.path.dirname(os.path.realpath(__file__))
 
 import sys
 sys.stderr = open(working_dir + r'\er.txt', 'w')
@@ -14,33 +14,57 @@ sys.stdout = open(working_dir + r'\log.txt', 'w')
 debug_logs_enabled = True
 
 
-def stable_diffusion_script(image, output_file=working_dir):
+def stable_diffusion_inpaint(image, output_file=working_dir):
     if debug_logs_enabled:
         pdb.gimp_message('script is running')
 
-    w, h, = image.active_layer.width, image.active_layer.height
-    x, y = image.active_layer.offsets
+    # get the mask of the layer as a pixel region
+    x, y, w, h = get_rect_of_layer(image.active_layer)
+    mask_region = get_layer_mask(image.active_layer, 0, 0, w, h)
 
-    try:
-        mask_region = image.active_layer.mask.get_pixel_rgn(0, 0, w, h, dirty=False)
-    except AttributeError:
-        exception_message = 'error: no mask found on selected layer'
-        pdb.gimp_message(exception_message)
-        raise Exception(exception_message)
+    # get the image of the layer as a pixel region
     image_copy = pdb.gimp_image_duplicate(image)
-    consolidated = image_copy.flatten()
-    consolidated_region = consolidated.get_pixel_rgn(x, y, w, h, dirty=False)
+    consolidated_image = image_copy.flatten()
+    consolidated_region = consolidated_image.get_pixel_rgn(x, y, w, h, dirty=False)
 
+    # save the images
     send_image_data(consolidated_region, mask_region, output_file)
 
+    # delete the copy
     pdb.gimp_image_delete(image_copy)
+
     if debug_logs_enabled:
         pdb.gimp_message('script finished')
 
 
+def stable_diffusion_img2img(image, output_file=working_dir):
+    if debug_logs_enabled:
+        pdb.gimp_message('script is running')
+
+    # code
+
+    if debug_logs_enabled:
+        pdb.gimp_message('script finished')
+
+
+def get_rect_of_layer(layer):
+    w, h, = layer.width, layer.height
+    x, y = layer.offsets
+    return x, y, w, h
+
+
+def get_layer_mask(layer, x, y, w, h):
+    try:
+        return layer.mask.get_pixel_rgn(x, y, w, h, dirty=False)
+    except AttributeError:
+        exception_message = 'error: no mask found on selected layer'
+        pdb.gimp_message(exception_message)
+        raise Exception(exception_message)
+
+
 def send_image_data(image, mask, output_file):
-    save_region_as_image(image, 0, 0, output_file + "\\image.png")
-    save_region_as_image(mask, 1, 2, output_file + "\\mask.png")
+    save_region_as_image(mask, 1, 2, output_file + r"\mask.png")
+    save_region_as_image(image, 0, 0, output_file + r"\image.png")
 
 
 def save_region_as_image(region, image_type, layer_type, path):
@@ -71,10 +95,22 @@ register(
     "SD inpaint",
     "RGB*, GRAY*",  # valid layer color type
     [
-        (PF_IMAGE, "image", "takes current image", None),
-        (PF_DIRNAME, "output", "Output folder", working_dir)
+        (PF_IMAGE, "image", "takes current image", None)
     ],
     [],
-    stable_diffusion_script, menu="<Image>/Tools/Paint Tools")
+    stable_diffusion_inpaint, menu="<Image>/Tools/Paint Tools")
+
+register(
+    "python-fu-SD-img2img",
+    "Send selected layer to stable diffusion webui",
+    "Send selected layer to stable diffusion webui",
+    "John-William Lebel", "John-William Lebel", "2023",
+    "SD img2img",
+    "RGB*, GRAY*",  # valid layer color type
+    [
+        (PF_IMAGE, "image", "takes current image", None)
+    ],
+    [],
+    stable_diffusion_img2img, menu="<Image>/Tools/Paint Tools")
 
 main()
--- scripts/gimp_inpainter.py
+++ scripts/gimp_inpainter.py
@@ -2,7 +2,9 @@ import os.path
 import gradio as gr
 import modules.scripts as scripts
 import sys
-import configparser
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 from modules.shared import opts
@@ -13,49 +15,41 @@ sys.path.append(base_dir)
 config_path = os.path.join(base_dir, 'user-config.cfg')
 
 
-def make_missing_directory_and_parent(destination_directory):
-    parent_directory = os.path.dirname(destination_directory)
-    if not os.path.exists(parent_directory):
-        parent_directory = os.path.dirname(destination_directory)
-        os.mkdir(parent_directory)
-    os.mkdir(destination_directory)
+# setting up the watchdogs
+class ImageHandler(FileSystemEventHandler):
+    file_path = os.path.join(base_dir, 'gimp_plugins', 'image.png')
+
+    def on_any_event(self, event):
+        print('image received!')
 
 
-def save_images_to_directory(images, backup_directory):
-    for image in images:
-        full_destination = os.path.join(backup_directory, image.already_saved_as)
-        destination_directory = os.path.dirname(full_destination)
-        if not os.path.exists(destination_directory):
-            make_missing_directory_and_parent(destination_directory)
-        image.save(full_destination)
-        print('saved "\033[94m' + os.path.basename(full_destination) + '\033[0m" to "\033[94m' + os.path.dirname(full_destination) + '\033[0m"')
+class MaskHandler(FileSystemEventHandler):
+    file_path = os.path.join(base_dir, 'gimp_plugins', 'mask.png')
+
+    def on_any_event(self, event):
+        print('mask received!')
 
 
-class AutoBackupScript(scripts.Script):
-    def __init__(self):
-        self.config = configparser.ConfigParser()
-        self.config.read(config_path)
-        self.initial_sd_backup_paths = self.config['PATHS']['backup_paths']
+image_handler = ImageHandler()
+mask_handler = MaskHandler()
+observer = Observer()
+observer.schedule(image_handler, ImageHandler.file_path)
+observer.schedule(mask_handler, MaskHandler.file_path)
+observer.start()
 
+
+class GimpScript(scripts.Script):
     def title(self):
-        return "Auto-backup"
+        return "Gimp-extension"
 
     def ui(self, is_img2img):
-        backup_folder_textbox = gr.Textbox(value=lambda: self.initial_sd_backup_paths, label='Backup folders', interactive=True)
-        return [backup_folder_textbox]
+        pass
 
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    def postprocess(self, p, res, *args):
-        with open(config_path, 'w') as config_file:
-            self.config['PATHS']['backup_paths'] = args[0]
-            self.config.write(config_file)
+    def run(self, p, *args):
+        pass
 
-        save_paths = args[0].split('|')
-        if opts.samples_save and not p.do_not_save_samples:
-            for save_path in save_paths:
-                save_path = save_path.strip()
-                if save_path == '':
-                    continue
-                save_images_to_directory(res.images, save_path)
+    def postprocess(self, p, res, *args):
+        pass  # send images back to gimp

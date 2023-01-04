4bb09d0f98be8998a8826dc700f936b519436cdb
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-01-04 04:04:19 -0500

    woups we might have implemented a lot of polling

--- scripts/gimp_inpainter.py
+++ scripts/gimp_inpainter.py
@@ -15,10 +15,14 @@ import gradio as gr
 image_args = {
     'img2img': None,
     'inpaint': None,
+    'inpaint_plus_mask': None,
+    'img2img_mask': None,
+    'inpaint_mask': None,
 }
 
 
 def update_image(tab, image):
+    print(f'{tab} demo.load: {image}')
     if image_args[tab] == image:
         return gr.update()
 
@@ -26,6 +30,7 @@ def update_image(tab, image):
 
 
 def update_image_args(tab, image):
+    print(f'{tab} image.change: {image}')
     if type(image) is dict:
         image_args[tab] = image['image']
     else:
@@ -35,10 +40,23 @@ def update_image_args(tab, image):
 @scripts_hijacker.hijack('create_ui')
 def create_ui_hijack(*args, original_function, **kwargs):
     with original_function(*args, **kwargs) as demo:
-        for tab in ['img2img', 'inpaint']:
-            image = generation_parameters_copypaste.paste_fields[tab]['init_img']
-            image.change(fn=update_image_args, inputs=[gr.State(tab), image], outputs=[])
-            demo.load(fn=update_image, inputs=[gr.State(tab), image], outputs=image, every=1)
+        tab = 'img2img'
+        image = generation_parameters_copypaste.paste_fields[tab]['init_img']
+        image.change(fn=update_image_args, inputs=[gr.State(tab), image], outputs=[])
+        demo.load(fn=update_image, inputs=[gr.State(tab), image], outputs=image, every=1)
+
+        tab = 'inpaint_plus_mask'
+        # image_mask = generation_parameters_copypaste.paste_fields['inpaint']['init_img']
+        # image_mask.change(fn=update_image_args, inputs=[gr.State(tab), image_mask], outputs=[])
+        # demo.load(fn=update_image, inputs=[gr.State(tab), image_mask], outputs=image_mask, every=1)
+
+        tab = 'inpaint'
+        image, mask = image_mask.parent.children[2:4]
+        image.change(fn=update_image_args, inputs=[gr.State(tab), image], outputs=[])
+        demo.load(fn=update_image, inputs=[gr.State(tab), image], outputs=image, every=1)
+        tab = 'inpaint_mask'
+        image.change(fn=update_image_args, inputs=[gr.State(tab), mask], outputs=[])
+        demo.load(fn=update_image, inputs=[gr.State(tab), mask], outputs=mask, every=1)
     demo_queue = demo.queue
     demo.queue = lambda *args, **kwargs: demo_queue()
     return demo
@@ -46,6 +64,9 @@ def create_ui_hijack(*args, original_function, **kwargs):
 
 def set_images_in_viewport(tab, image_path, mask_path=None):
     image_args[tab] = image_path
+    image_args[f'{tab}_mask'] = mask_path
+    if tab == 'inpaint':
+        image_args['inpaint_plus_mask'] = image_path
 
 
 gimp_plugin_path = os.path.join(base_dir, 'gimp_plugin')

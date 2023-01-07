994e1c02903e5ed6fc65c15f00912615f35215e7
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-01-07 16:06:32 -0500

    removed unused watchdogs lib

--- install.py
+++ install.py
@@ -2,5 +2,4 @@ import launch
 
 if not launch.is_installed('watchdog'):
     desc = 'requirements for gimporter'
-    launch.run_pip('install watchdog==2.2.1', desc)
     launch.run_pip('install websockets==10.4', desc)
